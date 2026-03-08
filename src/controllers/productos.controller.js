3cce2b420dfd9e53285b1430013ad6382fcb79c0
ARLOROBLE4 <maurosanromanmartinez82@gmail.com>
2026-03-08 12:14:13 -0600

    add: ednpoint actualizar productos

--- src/controllers/productos.controller.js
+++ src/controllers/productos.controller.js
@@ -20,6 +20,7 @@ export const getProductoById = async (req, res) => {
         res.status(500).json({ error: error.message });
     }
 };
+
 export const agregarProducto = async (req, res) => {
     try {
         if (!req.body.nombre) {
@@ -29,6 +30,27 @@ export const agregarProducto = async (req, res) => {
         const nuevo = await productoM.agregarProducto(req.body);
 
         res.status(201).json(nuevo);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
+
+export const actualizarProducto = async (req, res) => {
+    try {
+        const { id } = req.params; // Obtenemos el ID de la URL
+        
+        // Validación básica (opcional pero recomendada)
+        if (!req.body.nombre) {
+            return res.status(400).json({ message: "El campo nombre es requerido para actualizar" });
+        }
+
+        const filasAfectadas = await productoM.actualizarProducto(id, req.body);
+        
+        if (filasAfectadas === 0) {
+            return res.status(404).json({ message: "Producto no encontrado para actualizar" });
+        }
+        
+        res.status(200).json({ message: "Producto actualizado correctamente", id });
     } catch (error) {
         res.status(500).json({ error: error.message });
     }
