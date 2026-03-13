d3319d9e6f3a6bc6c8ae7cf728f956969cf43535
ARLOROBLE4 <maurosanromanmartinez82@gmail.com>
2026-03-12 23:30:31 -0600

    update api inicio

--- src/routes/inicio.routes.js
+++ src/routes/inicio.routes.js
@@ -4,7 +4,7 @@ import * as ctrl from '../controllers/inicio.controller.js';
 const router = Router();
 
 // Definición de endpoints para Inicio
-router.get('/:id', ctrl.getInicio);
+router.get('/', ctrl.getInicio);
 router.put('/:id', ctrl.actualizarInicio);
 
 export default router;
