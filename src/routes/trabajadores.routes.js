ce9dc14a05c851e7d72e39c4b55b740e887cfe00
ARLOROBLE4 <maurosanromanmartinez82@gmail.com>
2026-03-18 12:58:30 -0600

    update trabajadores

--- src/routes/trabajadores.routes.js
+++ src/routes/trabajadores.routes.js
@@ -5,8 +5,8 @@ import { verificarToken } from '../middlewares/middleware.js';
 const router = Router();
 
 // Todo el módulo de trabajadores debe estar protegido
-router.get('/', verificarToken, ctrl.getAllTrabajadores);
-router.get('/:id', verificarToken, ctrl.getTrabajadorById);
+router.get('/',  ctrl.getAllTrabajadores);
+router.get('/:id', ctrl.getTrabajadorById);
 router.post('/', verificarToken, ctrl.agregarTrabajador);
 router.put('/:id', verificarToken, ctrl.actualizarTrabajador);
 router.delete('/:id', verificarToken, ctrl.eliminarTrabajador);
