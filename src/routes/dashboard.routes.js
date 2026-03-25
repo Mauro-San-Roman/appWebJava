2c7cce6356de1133513b3d10fb7942d6bbe402a1
ARLOROBLE4 <maurosanromanmartinez82@gmail.com>
2026-03-24 20:34:57 -0600

    update

--- src/routes/dashboard.routes.js
+++ src/routes/dashboard.routes.js
@@ -1,11 +1,10 @@
 import { Router } from 'express';
 import * as ctrl from '../controllers/dashboard.controller.js';
-import { verificarToken } from '../middlewares/middleware.js'; 
 
 const router = Router();
 
 // Endpoints LIBERADOS para hacer pruebas directas en el navegador o Postman
-router.get('/bajo-stock', verificarToken, ctrl.obtenerStockBajo);
-router.get('/totales', verificarToken, ctrl.obtenerTotales);
+router.get('/bajo-stock', ctrl.obtenerStockBajo);
+router.get('/totales',  ctrl.obtenerTotales);
 
 export default router;
